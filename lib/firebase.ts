5479a6040305f76df2cabf84ff632cc39b116e82
PauloSarmento <paulo.cesar.sarmento@hotmail.com>
2025-12-22 12:58:57 -0300

    firebase

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,9 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.FIREBASE_APP_ID!,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
